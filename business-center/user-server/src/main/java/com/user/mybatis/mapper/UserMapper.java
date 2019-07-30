c98879e7f67c23f132ab5b4a10f54cb98b2c34bb
wangtong666168 <1165308717@qq.com>
2019-07-30 20:58:44 +0800

    1

--- business-center/user-server/src/main/java/com/user/mybatis/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.user.mybatis.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
-
-public interface UserMapper {
-    Integer selectUserCount();
-}
--- business-center/user-server/src/main/java/com/user/service/IUserService.java
+++
@@ -1,6 +0,0 @@
-package com.user.service;
-
-public interface IUserService {
-    Integer getUser() throws Exception;
-
-}
