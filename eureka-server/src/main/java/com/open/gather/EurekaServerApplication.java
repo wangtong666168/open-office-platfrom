563a366ce4ce8f276d7f8521b6cccb8d67529900
1165308717@qq.com <a15036667168>
2019-07-30 22:56:50 +0800

    1

--- eureka-server/src/main/java/com/open/gather/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/open/gather/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.open.gather.eurekaserver;
+package com.open.gather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ gather-base/auth-server/src/main/java/com/open/gather/AuthServerApp.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package com.open.gather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AuthServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApp.class, args);
+	}
+
+}
